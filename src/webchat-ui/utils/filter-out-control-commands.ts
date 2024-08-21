6d43d6782a7d5999cf62255c2488c8d705247a3a
Fabio Colajanni <f.colajanni@cognigy.com>
2024-08-21 16:51:00 +0200

    format file

--- src/webchat-ui/utils/filter-out-control-commands.ts
+++ src/webchat-ui/utils/filter-out-control-commands.ts
@@ -3,7 +3,10 @@ import { IMessage } from "../../common/interfaces/message";
 
 const EXCLUDE_CC_TYPES = ["acceptPrivacyPolicy", "setRating"];
 
-const getMessagesListWithoutControlCommands = (messages: (IMessage | IMessageEvent)[], types: string[] = EXCLUDE_CC_TYPES) => {
+const getMessagesListWithoutControlCommands = (
+	messages: (IMessage | IMessageEvent)[],
+	types: string[] = EXCLUDE_CC_TYPES,
+) => {
 	return messages.filter(message => {
 		if (message.data?._cognigy && (message.data._cognigy as any).controlCommands) {
 			return !(message.data._cognigy as any).controlCommands.some((controlCommand: any) =>
