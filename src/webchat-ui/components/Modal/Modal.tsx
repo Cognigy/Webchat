bc044bedfdc600ac5aefe80bf31f447f377607cb
vj-venkatesan <v.venkatesan@cognigy.com>
2025-03-17 17:33:28 +0100

    feat: add fade transition and overlay for modal component

--- src/webchat-ui/components/Modal/Modal.tsx
+++ src/webchat-ui/components/Modal/Modal.tsx
@@ -2,6 +2,20 @@ import React from "react";
 import styled from "@emotion/styled";
 import IconButton from "../presentational/IconButton";
 import CloseIcon from "../../assets/close-16px.svg";
+import { CSSTransition } from "react-transition-group";
+
+const Overlay = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	background: rgba(0, 0, 0, 0.5);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 9999;
+`;
 
 const StyledDialog = styled.dialog`
 	padding: 20px;
@@ -10,6 +24,15 @@ const StyledDialog = styled.dialog`
 	box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
 	background-color: #fff;
 	margin: 20px auto;
+	z-index: 99999;
+
+	&.fade-enter {
+		opacity: 0;
+	}
+	&.fade-enter-active {
+		opacity: 1;
+		transition: opacity 150ms;
+	}
 `;
 
 const ModalHeader = styled.div`
@@ -69,19 +92,24 @@ interface ModalProps {
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, footer, children }) => {
 	return (
-		<StyledDialog open={isOpen}>
-			<ModalHeader>
-				<ModalTitle>{title}</ModalTitle>
-				<CloseButton onClick={onClose}>
-					<CloseIcon />
-				</CloseButton>
-			</ModalHeader>
-			<DividerWrapper>
-				<Divider />
-			</DividerWrapper>
-			<ModalBody>{children}</ModalBody>
-			<ModalFooter>{footer}</ModalFooter>
-		</StyledDialog>
+		<>
+			{isOpen && <Overlay onClick={onClose} />}
+			<CSSTransition in={isOpen} timeout={150} mountOnEnter unmountOnExit classNames="fade">
+				<StyledDialog open={isOpen}>
+					<ModalHeader>
+						<ModalTitle>{title}</ModalTitle>
+						<CloseButton onClick={onClose}>
+							<CloseIcon />
+						</CloseButton>
+					</ModalHeader>
+					<DividerWrapper>
+						<Divider />
+					</DividerWrapper>
+					<ModalBody>{children}</ModalBody>
+					<ModalFooter>{footer}</ModalFooter>
+				</StyledDialog>
+			</CSSTransition>
+		</>
 	);
 };
 
