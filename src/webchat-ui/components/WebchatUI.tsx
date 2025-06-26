5138a9a66da09ccee65c96e8d8a0d668fb9df06b
Dmitrii Ostasevich <d.ostasevich@cognigy.com>
2025-06-26 12:32:53 +0200

    formatting

--- src/webchat-ui/components/WebchatUI.tsx
+++ src/webchat-ui/components/WebchatUI.tsx
@@ -1599,7 +1599,9 @@ export class WebchatUI extends React.PureComponent<
 						const hasText = !!message?.text?.trim();
 						const noControlCommands = !(message?.data?._cognigy as any)
 							?.controlCommands;
-						const hasAttachments = Array.isArray(message?.data?.attachments) && message.data.attachments.length > 0;
+						const hasAttachments =
+							Array.isArray(message?.data?.attachments) &&
+							message.data.attachments.length > 0;
 
 						return isUser && noControlCommands && (hasText || hasAttachments);
 					});
