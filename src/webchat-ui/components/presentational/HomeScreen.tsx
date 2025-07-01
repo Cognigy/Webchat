8546acea00c1fa82ad540d77985ff29c00b4f9ec
Sushmitha Sekar <sushmithasekar28@gmail.com>
2025-07-01 09:01:35 +0200

    Change homescreen close button colors

--- src/webchat-ui/components/presentational/HomeScreen.tsx
+++ src/webchat-ui/components/presentational/HomeScreen.tsx
@@ -91,23 +91,23 @@ const HomeScreenHeader = styled.div({
 });
 
 const HomeScreenHeaderIconButton = styled(IconButton)(({ theme }) => ({
-	color: theme.primaryContrastColor,
+	color: theme.white,
 	borderRadius: 4,
 	padding: 0,
 	margin: 4,
 	svg: {
-		fill: theme.primaryContrastColor,
+		fill: theme.white,
 		width: 16,
 		height: 16,
 	},
 	"&.active, &:hover": {
-		color: theme.primaryContrastColor,
-		fill: theme.primaryContrastColor,
+		color: theme.white,
+		fill: theme.white,
 	},
 	"&:focus-visible": {
 		outline: `2px solid ${theme.primaryColorFocus}`,
 		outlineOffset: 2,
-		boxShadow: `0 0 0 4px ${theme.primaryContrastColor}`,
+		boxShadow: `0 0 0 4px ${theme.white}`,
 	},
 }));
 
