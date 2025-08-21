727790ac6ae2246352908ad77b280530e4f04f9b
Dmitrii Ostasevich <d.ostasevich@cognigy.com>
2025-08-21 12:17:22 +0200

    remove compressed assets generation

--- webpack.production.js
+++ webpack.production.js
@@ -4,38 +4,11 @@ const { version } = require("./package.json");
 const config = require("./webpack.config");
 
 const TerserPlugin = require("terser-webpack-plugin");
-const zlib = require("zlib");
-const CompressionPlugin = require("compression-webpack-plugin");
 
 config.mode = "production";
 config.plugins.push(
 	new webpack.BannerPlugin({
 		banner: `[file] v${version}\nhttps://github.com/Cognigy/Webchat/tree/v${version}\nhttps://github.com/Cognigy/Webchat/tree/v${version}/OSS_LICENSES.txt`,
-	}),
-);
-
-config.plugins.push(
-	new CompressionPlugin({
-		filename: "[path][base].gz",
-		algorithm: "gzip",
-		test: /\.(js|css|html|svg|ts|tsx)$/,
-		threshold: 10240,
-		minRatio: 0.8,
-	}),
-);
-
-config.plugins.push(
-	new CompressionPlugin({
-		filename: "[path][base].br",
-		algorithm: "brotliCompress",
-		test: /\.(js|css|html|svg|ts|tsx)$/,
-		compressionOptions: {
-			params: {
-				[zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-			},
-		},
-		threshold: 10240,
-		minRatio: 0.8,
 	}),
 );
 
