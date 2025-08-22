834e623dfe6f465a2f7849275e2f2b92d6070594
Dmitrii Ostasevich <d.ostasevich@cognigy.com>
2025-08-22 12:08:04 +0200

    reduce output noise

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,13 +5,5 @@ export default defineConfig({
 	viewportHeight: 800,
 	e2e: {
 		baseUrl: "http://localhost:8787/",
-		setupNodeEvents(on) {
-			on("task", {
-				log(message) {
-					console.log(message);
-					return null;
-				},
-			});
-		},
 	},
 });
