3dacd4056ed8fc574243d6e9c94bcb7af68aa5a1
Sushmitha Sekar <sushmithasekar28@gmail.com>
2025-12-03 10:14:14 +0100

    Add tests to check empty message rendering

---
+++ cypress/e2e/emptyMessage.cy.ts
@@ -0,0 +1,35 @@
+describe('Empty Message', () => {
+	beforeEach(() => {
+		cy.visitWebchat();
+
+		cy.initMockWebchat({
+			settings: {
+				behavior: {
+					progressiveMessageRendering: true,
+					collateStreamedOutputs: false,
+				},
+			},
+		});
+
+		cy.openWebchat().startConversation();
+	});
+
+	it('should not render messages with empty text', () => {
+		// Simulate adding a message with empty text
+		cy.receiveMessage("Hi");
+		cy.receiveMessage(" ");
+		cy.receiveMessage("How can I help you?");
+		cy.wait(1000);
+		cy.get("article").should('have.length', 2);
+	});
+
+	it('should not render messages with only escape sequences', () => {
+		// Simulate adding a message with only escape sequences
+		cy.receiveMessage("Hi");
+        cy.receiveMessage("\n\t  \n");               
+		cy.receiveMessage("How can I help you?");
+        cy.receiveMessage(" \n\n");   
+		cy.wait(1000); // wait for rendering        
+		cy.get("article").should('have.length', 2);
+	});
+});
